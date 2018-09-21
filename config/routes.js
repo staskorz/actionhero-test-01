c1d4f67f14819b07f87ff98e162b90bd6b02d8b3
staskorz <staskorz@gmail.com>
2018-09-21 12:36:02 +0300

    Configured routes

--- config/routes.js
+++ config/routes.js
@@ -1,30 +1,25 @@
 exports["default"] = {
-  routes: api => {
-    return {
-      /* ---------------------
-      routes.js
+  routes: () => ({
+    get: [
+      { path: "/users", action: "usersList" },
+      { path: "/comments/:userName/:title", action: "commentsView" },
+      { path: "/post/:userName/:title", action: "postView" },
+      { path: "/posts/:userName", action: "postsList" },
+    ],
 
-      For web clients (http and https) you can define an optional RESTful mapping to help route requests to actions.
-      If the client doesn't specify and action in a param, and the base route isn't a named action, the action will attempt to be discerned from this routes.js file.
+    post: [
+      { path: "/authenticate", action: "authenticate" },
+      { path: "/user", action: "userAdd" },
+      { path: "/comment/:userName/:title", action: "commentAdd" },
+      { path: "/post/:userName", action: "postAdd" },
+    ],
 
-      Learn more here: http://www.actionherojs.com/docs/#routes
+    put: [{ path: "post/:userName/:title", action: "postEdit" }],
 
-      examples:
-
-      get: [
-        { path: '/users', action: 'usersList' }, // (GET) /api/users
-        { path: '/search/:term/limit/:limit/offset/:offset', action: 'search' }, // (GET) /api/search/car/limit/10/offset/100
-      ],
-
-      post: [
-        { path: '/login/:userID(^\\d{3}$)', action: 'login' } // (POST) /api/login/123
-      ],
-
-      all: [
-        { path: '/user/:userID', action: 'user', matchTrailingPathParts: true } // (*) /api/user/123, api/user/123/stuff
-      ]
-
-      ---------------------- */
-    }
-  },
+    delete: [
+      { path: "/user/userName", action: "userDelete" },
+      { path: "/comment/:userName/:title/:commentId", action: "commentDelete" },
+      { path: "/post/:userName/:title", action: "postDelete" },
+    ],
+  }),
 }
